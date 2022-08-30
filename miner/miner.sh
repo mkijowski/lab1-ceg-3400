8fcce8a7ef762f9751ea1648a8d4147c4b142a5a
Matt Kijowski <matthew.kijowski@gmail.com>
2022-08-30 10:15:25 -0400

    uopdated quizdata and added miner starter script

---
+++ miner/miner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Prints all words in provided dictionary
+## (you might want to find a bigger dictionary)
+for i in $(cat ../data/dictionary); do
+  printf $i
+done
+
+
+## prints all numbers between 100 and 105
+for i in $(seq 100 105); do
+  printf $i
+done
+
+
